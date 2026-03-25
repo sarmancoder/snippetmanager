497fab34dd5b4d9310ae7a896234db3dd23d0129
Raul <sarmancoder@gmail.com>
2026-03-25 12:45:18 +0100

    Dejar puesto el popup de ia para modificar snippets con IA, (no funciona)

--- lib/HolyGrailLayout.dart
+++ lib/HolyGrailLayout.dart
@@ -1,38 +1,47 @@
 import 'package:aisnippets/components/files_drawer.dart';
+import 'package:aisnippets/components/ia/PopupContent.dart';
 import 'package:aisnippets/components/snippets_drawer.dart';
 import 'package:aisnippets/components/snippets_web_page.dart';
+import 'package:aisnippets/providers/snippet_file.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:popover/popover.dart';
 
 class HolyGrailLayout extends StatelessWidget {
   const HolyGrailLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Stack(
       children: [
-        // --- SIDEBAR IZQUIERDO PERSISTENTE ---
-        Container(
-          width: 250,
-          color: Colors.white,
-          child: FilesDrawerLoader(),
+        Row(
+          children: [
+            // --- SIDEBAR IZQUIERDO PERSISTENTE ---
+            Container(
+              width: 250,
+              color: Colors.white,
+              child: FilesDrawerLoader(),
+            ),
+
+            // Divisor visual
+            const VerticalDivider(width: 1, thickness: 1),
+
+            // --- CONTENIDO PRINCIPAL (Flexible para ocupar el resto) ---
+            Expanded(
+              child: Container(
+                color: Colors.grey[100],
+                child: SnippetsWebPage(),
+              ),
+            ),
+
+            // Divisor visual
+            const VerticalDivider(width: 1, thickness: 1),
+
+            // --- PANEL DERECHO PERSISTENTE ---
+            SnippetsDrawer(),
+          ],
         ),
-
-        // Divisor visual
-        const VerticalDivider(width: 1, thickness: 1),
-
-        // --- CONTENIDO PRINCIPAL (Flexible para ocupar el resto) ---
-        Expanded(
-          child: Container(
-            color: Colors.grey[100],
-            child: SnippetsWebPage()
-          ),
-        ),
-
-        // Divisor visual
-        const VerticalDivider(width: 1, thickness: 1),
-
-        // --- PANEL DERECHO PERSISTENTE ---
-        SnippetsDrawer(),
+        IaButton(),
       ],
     );
   }
