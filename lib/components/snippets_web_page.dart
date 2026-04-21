0a38fb29cf356fba7ea1acec79aaff5cafeae42f
Raúl Contreras <sarmancoder@outlook.es>
2026-04-21 20:22:26 +0200

    Problemas de carga arreglados

--- lib/components/snippets_web_page.dart
+++ lib/components/snippets_web_page.dart
@@ -93,7 +93,7 @@ class _SnippetsWebPageState extends ConsumerState<SnippetsWebPage> {
   ) async {
     if (_webViewController == null) return;
     final jsonDetail = jsonEncode(detail);
-    try { 
+    try {
       await _webViewController!.evaluateJavascript(
         source:
             """
@@ -105,17 +105,16 @@ class _SnippetsWebPageState extends ConsumerState<SnippetsWebPage> {
     """,
       );
     } on MissingPluginException {
-    
-    } catch(exception) {
+    } catch (exception) {
       print(exception.toString());
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    var active = ref.watch(
-      snippetFileProvider.select((a) => a?.activeSnippet)
-    );
+    var theme = Theme.of(context);
+
+    var active = ref.watch(snippetFileProvider.select((a) => a?.activeSnippet));
     ref.listen(snippetFileProvider.select((a) => a?.activeSnippet), (
       old,
       curr,
@@ -131,75 +130,81 @@ class _SnippetsWebPageState extends ConsumerState<SnippetsWebPage> {
 
     ref.listen(uiBrightnessProvider, (old, curr) {
       _dispatchCustomEvent('toggleDark', {
-        'dark': curr == Brightness.dark ? 1 : 0
+        'dark': curr == Brightness.dark ? 1 : 0,
       });
     });
 
-    return Column(
+    return IndexedStack(
+      index: active != null ? 1 : 0,
       children: [
-        Expanded(
-          child: Container(
-            margin: EdgeInsets.only(left: 10),
-            child: active == null ? Container() : InAppWebView(
-              initialUrlRequest: URLRequest(
-                url: WebUri(snippetsWebEditorAddress),
-              ),
-
-              onWebViewCreated: (controller) {
-                _webViewController = controller;
-                debugPrint('WebView creado');
-
-                controller.addJavaScriptHandler(
-                  handlerName: 'updateSnippet',
-                  callback: (args) {
-                    // Obtener siempre el estado actual, no usar la referencia capturada
-                    final currentState = ref.read(snippetFileProvider);
-                    final currentActive = currentState?.activeSnippet;
-                    
-                    if (currentActive == null) return;
-                    
-                    var newSnippet = jsonDecode(args[0]);
-                    var snippet = Snippet(
-                      prefix: newSnippet['prefix'] ?? '',
-                      description: newSnippet['description'] ?? '',
-                      body: (newSnippet['body'] is List ? newSnippet['body'].join('\n') : newSnippet['body'] ?? ''),
-                      key: currentActive.key,
-                      scope: newSnippet['scope'] ?? '',
-                    );
-                    if (!snippet.equals(currentActive)) {
-                      ref.read(snippetFileProvider.notifier).setEditingSnippet(snippet);
-                    }
-                  },
+        Center(
+          child: Text(
+            "No hay ningún snippet abierto actualmente",
+            style: theme.textTheme.headlineLarge,
+          ),
+        ),
+        InAppWebView(
+          initialUrlRequest: URLRequest(
+            url: WebUri(snippetsWebEditorAddress),
+          ),
+            
+          onWebViewCreated: (controller) {
+            _webViewController = controller;
+            debugPrint('WebView creado');
+            
+            controller.addJavaScriptHandler(
+              handlerName: 'updateSnippet',
+              callback: (args) {
+                // Obtener siempre el estado actual, no usar la referencia capturada
+                final currentState = ref.read(snippetFileProvider);
+                final currentActive = currentState?.activeSnippet;
+            
+                if (currentActive == null) return;
+            
+                var newSnippet = jsonDecode(args[0]);
+                var snippet = Snippet(
+                  prefix: newSnippet['prefix'] ?? '',
+                  description: newSnippet['description'] ?? '',
+                  body: (newSnippet['body'] is List
+                      ? newSnippet['body'].join('\n')
+                      : newSnippet['body'] ?? ''),
+                  key: currentActive.key,
+                  scope: newSnippet['scope'] ?? '',
                 );
+                if (!snippet.equals(currentActive)) {
+                  ref
+                      .read(snippetFileProvider.notifier)
+                      .setEditingSnippet(snippet);
+                }
               },
-
-              /*onConsoleMessage: (controller, consoleMessage) {
-                  print("JS Console: ${consoleMessage.message}");
-                },*/
-              onLoadStop: (controller, url) async {
-                // Insertar snippet con retardo
-                if (firstLoaded) return;
-                await Future.delayed(Duration(milliseconds: 200));
-                var curr = active;
-                _dispatchCustomEvent('insertSnippet', {
-                  "prefix": curr.prefix,
-                  "description": curr.description,
-                  "body": curr.body.split('\n'),
-                  "scope": curr.scope,
-                });
-
-                setState(() {
-                  firstLoaded = true;
-                });
-              },
-
-              initialSettings: InAppWebViewSettings(
-                underPageBackgroundColor: const Color(0x00000000),
-                forceDark: ForceDark.AUTO,
-                javaScriptEnabled: true,
-                useHybridComposition: true,
-              ),
-            ),
+            );
+          },
+            
+          /*onConsoleMessage: (controller, consoleMessage) {
+              print("JS Console: ${consoleMessage.message}");
+            },*/
+          onLoadStop: (controller, url) async {
+            // Insertar snippet con retardo
+            if (firstLoaded) return;
+            if (active == null) return;
+            await Future.delayed(Duration(milliseconds: 200));
+            _dispatchCustomEvent('insertSnippet', {
+              "prefix": active.prefix,
+              "description": active.description,
+              "body": active.body.split('\n'),
+              "scope": active.scope,
+            });
+            
+            setState(() {
+              firstLoaded = true;
+            });
+          },
+            
+          initialSettings: InAppWebViewSettings(
+            underPageBackgroundColor: const Color(0x00000000),
+            forceDark: ForceDark.AUTO,
+            javaScriptEnabled: true,
+            useHybridComposition: true,
           ),
         ),
       ],
