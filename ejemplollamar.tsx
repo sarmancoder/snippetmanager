f2c13cbcaf94f2689b0e86b5934876082f128b87
Raul <sarmancoder@gmail.com>
2026-07-12 16:58:28 +0200

    Inicio proyecto

---
+++ ejemplollamar.tsx
@@ -0,0 +1,24 @@
+/*import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState('')
+  return (
+    <main>
+      <p>Holaa</p>
+      <button onClick={() => {
+        invoke<string>('greet', {name: 'Raúl'}).then((r) => {
+          console.log(r)
+          setMessage(r)
+        })
+      }}>
+        llamar
+      </button>
+      <span>{message}</span>
+    </main>
+  );
+}
+
+export default App;
+*/
