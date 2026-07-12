59f60479243150e00bc20d28d721e30fb15a6e01
Raul <sarmancoder@gmail.com>
2026-07-12 20:40:46 +0200

    Poner de color blanco el boton de theme toggle

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -5,7 +5,7 @@ export default function ModeToggle() {
   const { theme, setTheme } = useTheme()
 
   return (
-    <div onClick={() => setTheme(theme == 'light' ? 'dark' : 'light')} className="cursor-pointer text-2xl text-theme">
+    <div onClick={() => setTheme(theme == 'light' ? 'dark' : 'light')} className="cursor-pointer text-2xl text-white">
       {theme == 'light' && <FaSun />}
       {theme == 'dark' && <FaMoon />}
       <span className="sr-only">Toggle theme</span>
